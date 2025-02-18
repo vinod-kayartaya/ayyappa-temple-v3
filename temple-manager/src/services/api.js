db7a897e8c94e741b71deaa1c6fab0eeee47494a
Vinod Kumar Kayartaya <vinod@vinod.co>
2025-02-18 19:39:15 +0530

    updated base_url for api

--- temple-manager/src/services/api.js
+++ temple-manager/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
 
 export const loginUser = async (credentials) => {
   try {
